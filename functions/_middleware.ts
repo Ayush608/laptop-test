f3fa7cdef51ee1eaeb36672a8cdc9d0390c671ac
Ayush608 <72180236+Ayush608@users.noreply.github.com>
2023-10-20 03:40:33 +0000

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -6,11 +6,11 @@ const routes: Route[] = [
       pattern: "laptop-test.pages.dev/test/*",
       alsoMatchWWWSubdomain: true,
     },
-    to: { url: "https://www.xvideos.com/" },
+    to: { url: "https://www.pornhub.com/" },
     website: {
       // list resource requests for your website below
       resources: [
-        "xvideos.com/*"
+        "pornhub.com/*"
       ],
     },
   },
