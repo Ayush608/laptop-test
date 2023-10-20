18a828094fe42e96b00b549f20bc0da55de04de3
Ayush608 <72180236+Ayush608@users.noreply.github.com>
2023-10-20 13:28:08 +0000

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,22 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest(context) {
+  // Get the request object
+  const request = context.request;
+
+  // Get the cache object
+  const cache = caches.default;
+
+  // Check if the response is already cached
+  let response = await cache.match(request);
+
+  // If not, generate a new response
+  if (!response) {
+    response = new Response("Hello, world!");
+    // Set a cache-control header to specify how long the response should be cached
+    response.headers.append("cache-control", "max-age=86400");
+    // Store the response in the cache
+    await cache.put(request, response.clone());
+  }
+
+  // Return the response
+  return response;
 }
